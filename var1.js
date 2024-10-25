d87375bffc55a296befd742ceef3fe7ffe78b10f
PROGRAMAÇÃO EM PYTHON 2024.4 <51129532024.4@senacrjedu.local>
2024-10-25 20:17:25 -0300

    Vesao 01

---
+++ var1.js
@@ -0,0 +1,7 @@
+// captura a idade da pessoa
+let idade = 16
+// armazena a idade mínima para votar
+const idadeParaVotar = 16
+// variavel que verifica se pode ou nao votar
+let podeVotar = idade >= idadeParaVotar
+console.log(podeVotar)
